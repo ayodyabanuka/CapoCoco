579e3e4ff32fe73b22463e2ba4d82b45c165aa22
ayodyabanuka <ayodyabanukafernando@gmail.com>
2023-11-06 20:28:55 +0530

    updated

--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,6 +1,6 @@
 const ContactUs = () => {
        return (
-              <div>
+              <div className="mx-10">
                      <div className="hidden md:flex flex-col max-w-[1920px] gap-7 my-10 rounded-3xl h-[1097px] text-white justify-center items-center px-0 lg:px-16 bg-[#FFF4E2]">
 
                      </div >
